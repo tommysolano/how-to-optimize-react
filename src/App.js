b8d980e3f1bc7df9ea22cb8b04241d73057256df
tommy <tommysolano18@hotmail.com>
2021-11-11 17:23:53 -0500

    aplicando useCallback

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import Title from './components/Title'
 import MyForm from './components/Forms/MyForm'
 import MyList from './components/Lists/MyList'
 
 function App() {
   const [valores, setValores] = useState([])
-  const handleSubmit = (values) => {
-    setValores([
-      ...valores,
+  const handleSubmit = useCallback((values) => {
+    setValores(data => ([
+      ...data,
       values
-    ])
-  }
+    ]))
+  }, [])
 
   return (
     <div>
